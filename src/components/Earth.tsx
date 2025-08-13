68bf5c94e70ab1d0c45874032a71cb2449079304
Divax <diya27divax1234@gmail.com>
2025-08-13 12:23:25 +0530

    textures test

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -5,10 +5,10 @@ import { PlanetCanvas } from "./Planet";
 export function EarthCanvas() {
   return (
     <PlanetCanvas
-      mapSrc="/textures/earth/earth_day_1k.jpg"
-      specularSrc="/textures/earth/earth_spec_1k.jpg"
-      cloudsSrc="/textures/earth/earth_clouds_1k.jpg"
-      cloudsAlphaSrc="/textures/earth/earth_clouds_trans_1k.jpg"
+      mapSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_clouds_1k.jpg"
+      specularSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_spec_1k.jpg"
+      cloudsSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_clouds_1k.jpg"
+      cloudsAlphaSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_clouds_trans_1k.jpg"
       tiltDeg={23.4}
       speed={0.15}
       camera={[0, 0, 2.9]}
