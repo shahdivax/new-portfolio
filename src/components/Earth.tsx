c3f660ab23aff32d9a29cd60dc720bcc4deff798
Divax <diya27divax1234@gmail.com>
2025-08-13 12:47:21 +0530

    earth update

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -5,7 +5,7 @@ import { PlanetCanvas } from "./Planet";
 export function EarthCanvas() {
   return (
     <PlanetCanvas
-      mapSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_clouds_1k.jpg"
+      mapSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_day_1k.jpg"
       specularSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_spec_1k.jpg"
       cloudsSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_clouds_1k.jpg"
       cloudsAlphaSrc="https://huggingface.co/datasets/diabolic6045/divax-portfolio/resolve/main/public/textures/earth/earth_clouds_trans_1k.jpg"
